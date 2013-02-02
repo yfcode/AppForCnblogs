cdc12c3f1e6306c0016815226c300898c7dbb9a3
boping <lbp0200@hotmail.com>
2013-02-02 22:01:38 +0800

    init

---
+++ src/com/cnblogs/AppForCnblogsActivity.java
@@ -0,0 +1,13 @@
+package com.cnblogs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppForCnblogsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
